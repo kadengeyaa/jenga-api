bb72bccac53ace97a78a920025900b1ca4ed7da2
Kadenge Yaa <kadengeyaa@gmail.com>
2022-11-21 00:36:38 +0300

    Completed send money to mobile wallet

--- src/interface/sendMoney.interface.ts
+++ src/interface/sendMoney.interface.ts
@@ -6,3 +6,10 @@ export interface SendMoneyWithinEquityBankResponse {
   message: ResponseMessage;
   data: { transactionId: string; status: 'SUCCESS' };
 }
+
+export interface SendMoneyToMobileWalletResponse {
+  status: boolean;
+  code: -1;
+  message: string;
+  transactionId: '7878787878787879';
+}
--- src/service/sendMoney.service.ts
+++ src/service/sendMoney.service.ts
@@ -4,7 +4,10 @@ import axios from 'axios';
 import { URLConfig } from '../config/url.config.js';
 import { LoggerUtil } from '../util/logger.js';
 import { AuthOptions } from '../interface/auth.interface.js';
-import { SendMoneyWithinEquityBankResponse } from '../interface/sendMoney.interface.js';
+import {
+  SendMoneyToMobileWalletResponse,
+  SendMoneyWithinEquityBankResponse,
+} from '../interface/sendMoney.interface.js';
 import { CountryCode, CurrencyCode } from '../interface/general.interface.js';
 import moment from 'moment';
 
@@ -67,6 +70,78 @@ export abstract class SendMoneyService {
     ).data;
 
     LoggerUtil.logger.log('jenga-sendMoneyWithinEquityBank %o', response);
+
+    return response;
+  }
+
+  static async sendToMobileWallet(
+    data: {
+      source: {
+        countryCode: CountryCode;
+        name: string;
+        accountNumber: string;
+      };
+      destination: {
+        type: 'mobile';
+        countryCode: CountryCode;
+        name: string;
+        mobileNumber: string;
+        walletName: 'Mpesa' | 'Airtel' | 'Equitel';
+      };
+      transfer: {
+        type: 'MobileWallet';
+        amount: number;
+        currencyCode: CurrencyCode;
+        reference: string;
+        date: Date;
+        description: string;
+      };
+    },
+    options?: {
+      authOptions?: AuthOptions;
+      signOptions?: SignOptions;
+    },
+  ): Promise<SendMoneyToMobileWalletResponse> {
+    const {
+      source: { accountNumber },
+      destination: { walletName },
+      transfer: { amount, currencyCode, reference, date: dateToFormat },
+    } = data;
+
+    const date = moment.utc(dateToFormat).format('YYYY-MM-DD');
+
+    const signature = SignService.getSignature(
+      walletName === 'Equitel'
+        ? `${amount}${currencyCode}${reference}${accountNumber}`
+        : `${accountNumber}${amount}${currencyCode}${reference}`,
+      options?.signOptions,
+    );
+
+    const { accessToken } = await AuthService.getAuth(options?.authOptions);
+
+    const rawResponse: Record<string, unknown> = (
+      await axios({
+        method: 'post',
+        url: URLConfig.kSendMoneyToMobileWallet,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          signature: signature,
+          'Content-Type': 'application/json',
+        },
+        data: {
+          ...data,
+          ...{ transfer: { ...data.transfer, ...{ date } } },
+        },
+      })
+    ).data;
+
+    rawResponse['transactionId'] = rawResponse['transactionId '];
+
+    delete rawResponse['transactionId '];
+
+    const response: SendMoneyToMobileWalletResponse = rawResponse as unknown as SendMoneyToMobileWalletResponse;
+
+    LoggerUtil.logger.log('jenga-sendMoneyToMobileWallet %o', response);
 
     return response;
   }
